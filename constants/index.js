2cd9d6c3808729391e987999ecca3df0cdd60c36
Vishnu Rathore <vr8043836@gamil.com>
2022-07-18 22:01:27 +0530

    initial commit

---
+++ constants/index.js
@@ -0,0 +1,4 @@
+// Address of the NFT Contract that you deployed
+const CRYPTO_DEVS_NFT_CONTRACT_ADDRESS = "0x802c48E412DDc6780E2A315CaFAad27f43276c3d";
+
+module.exports = { CRYPTO_DEVS_NFT_CONTRACT_ADDRESS };
